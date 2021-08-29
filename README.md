61e30dabbab1ec8526ede374f21e59f66e10b261
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2021-08-30 02:19:07 +0530

    project code for employee seperated finally

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# smartCityVaranasiEmployee
-User Interface of Employee
+# smartVaranasiNode
+
+Step 1:
+fork the repo
+
+
+Step 2:
+git clone [repo url]
+
+Step 3:
+npm install
+
+Step 4:
+npm run dev -> starts the project
---
+++ route/authRoute.js
@@ -0,0 +1,59 @@
+import express from 'express';
+const router = express.Router();
+import {OAuth2Client} from 'google-auth-library';
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+import Users from '../model/user.js';
+
+
+// getting login URL
+// /auth/login
+let userid;
+router.post('/login', (req, res) => {
+    let token = req.body.token;
+    
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.GOOGLE_CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
+        });
+        const payload = ticket.getPayload();
+        userid = payload['sub'];
+        let userData =  await Users.findOne({googleId: userid});
+        if (userData == null) {
+            // create a new user
+            if (userData == null) {
+                let newUser = new Users({
+                    firstName: payload['given_name'],
+                    lastName: payload['family_name'],
+                    image: payload['picture'],
+                    email: payload['email'],
+                    designation: "Employee"
+                });
+                await newUser.save();
+            }
+            
+        }
+        console.log(userData);
+      }
+      verify()
+      .then(()=>{
+          res.cookie('session-token', token);
+          res.cookie('user-id', userid);
+          res.send('success')
+      })
+      .catch(console.error);
+
+})
+
+// logout
+// /auth/logout
+router.get('/logout', (req, res) => {
+    console.log(req.cookies['session-token']);
+    res.clearCookie('session-token');
+    res.send('successfol logout');
+})
+
+// getting the current user
+
+
+export default router;
