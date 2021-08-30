816308d8ab8340d4928891a680f87ca14d1b489d
Aman Singh <aman.singh.civ19@itbhu.ac.in>
2021-08-30 10:15:10 +0530

    fixes2

--- public/javascript/googleMap.js
+++ public/javascript/googleMap.js
@@ -9,7 +9,7 @@ async function getAllBins(url){
 //use the data to create markers on the map
 var data;
 if (localStorage.getItem('mapData') === null) {
-  data = getAllBins("http://localhost:4000/get").then((allBins) => {
+  data = getAllBins(location.origin+"/get").then((allBins) => {
     localStorage.setItem('mapData', JSON.stringify(allBins));
     console.log('data addded to LS');
   })
@@ -61,12 +61,12 @@ function initMap(){
       }
       
       marker.addListener('click', ()=> {
-        location.href = `http://localhost:4000/waste/${features[i].id}`;
+        location.href = location.origin+`/waste/${features[i].id}`;
       });
     }
 
     setTimeout(()=> {
-      data = getAllBins("http://localhost:4000/get").then((allBins) => {
+      data = getAllBins(location.origin+"/get").then((allBins) => {
         localStorage.setItem('mapData', JSON.stringify(allBins));
         console.log('data updated to LS');
         // initMap();
